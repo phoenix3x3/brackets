8b3bea59d60a6090c4a6546d72f7ce5e9372b99f
phoenix3x3 <fenix3x3@mail.ru>
2019-03-17 18:47:55 +0300

    feat: - add solution of task in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+		let bufferStr = [];
+		str.split("").forEach(function(element, index) {
+			if(index == 0) {
+				bufferStr.push(element);
+				return 0;
+			}
+			let currentBracket = bracketsConfig.find(bracketType =>
+				bufferStr[bufferStr.length - 1] == bracketType[0]);
+			currentBracket = currentBracket && currentBracket[1];
+			if(element != currentBracket){
+				bufferStr.push(element);
+			}
+			
+			else{
+				bufferStr.pop();
+			}
+		});
+	
+		if(bufferStr.length == 0) return true;
+		return false;
+		
+	}
